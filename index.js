c20ad3e0af65a98f68e31186d6ef10e75907e4b6
Rayhan-Rony <jobayerahmedrony@gmail.com>
2022-10-27 02:23:04 +0600

    added category

--- index.js
+++ index.js
@@ -4,9 +4,13 @@ const cors = require('cors')
 app.use(cors())
 const port = process.env.PORT || 5000;
 
+const courses = require('./data/courses.json')
 
 
 app.get('/', (req, res) => {
     res.send('Server Is Running')
+})
+app.get('/courses', (req, res) => {
+    res.send(courses)
 })
 app.listen(port, () => { console.log('server is running', port) })
